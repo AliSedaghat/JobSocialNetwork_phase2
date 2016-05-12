61cfeac9cd9f87d3f4005c5d3ed089102e1e4fd0
SAEED <developer.saeedzhiany@gmail.com>
2016-05-12 20:07:20 +0430

    file has been created and getall method has been overrode

---
+++ src/main/java/dao/TeamDAOImpl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import databasemodels.Team;
+import java.util.List;
+
+/**
+ *
+ * @author SAEED
+ */
+public class TeamDAOImpl extends GenericDAOImpl<Team, Integer>{
+
+    public TeamDAOImpl(Class<Team> type) {
+        super(type);
+    }
+
+    @Override
+    List<Team> getall() {
+        return factory.getCurrentSession()
+                .getNamedQuery("Team.findAll").list();
+    }
+    
+}
