343781cb87d0cd0873d0916f7410fee5b6819145
SAEED <developer.saeedzhiany@gmail.com>
2016-05-12 20:46:32 +0430

    file has been created and getall method has been overrode

---
+++ src/main/java/dao/AccountDAOImpl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import databasemodels.Account;
+import java.util.List;
+
+/**
+ *
+ * @author SAEED
+ */
+public class AccountDAOImpl extends GenericDAOImpl<Account, Integer>{
+
+    public AccountDAOImpl(Class<Account> type) {
+        super(type);
+    }
+
+    @Override
+    List<Account> getall() {
+        return factory.getCurrentSession()
+                .getNamedQuery("Account.findAll").list();
+    }
+    
+}
