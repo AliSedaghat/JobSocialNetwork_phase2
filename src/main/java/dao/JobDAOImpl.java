cbf938e464560d685ae3babfa66f14ab57fa51ea
SAEED <developer.saeedzhiany@gmail.com>
2016-05-16 23:32:52 +0430

    this Dao has been created and methods have been implemented

---
+++ src/main/java/dao/JobDAOImpl.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import databasemodels.Job;
+import java.util.List;
+
+/**
+ *
+ * @author SAEED
+ */
+public class JobDAOImpl extends GenericDAOImpl<Job, Integer>{
+
+    public JobDAOImpl(Class<Job> type) {
+        super(type);
+    }
+
+    @Override
+    public List<Job> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer getCountAll() {
+        return (Integer) factory.getCurrentSession()
+                .getNamedQuery("Job.count").list().get(0);
+    }
+    
+}
