5be8428f7a1dabac139368b90f987c246e5d4474
SAEED <developer.saeedzhiany@gmail.com>
2016-05-19 19:48:59 +0430

    Changing password ability has been added

---
+++ src/main/java/controllers/ChangePasswordController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import databasemodels.Account;
+import entitymanager.AccountManagerImpl;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author SAEED
+ */
+@WebServlet(name = "ChangePasswordController", urlPatterns = {"/ChangePasswordController"})
+public class ChangePasswordController extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        final String curPassword = request.getParameter("oldPassword");
+        final String newPassword = request.getParameter("newPassword");
+        final String repeatPassword = request.getParameter("repeatPassword");
+        
+        String errorMessages = "";
+        if(curPassword.equals("")){
+            errorMessages += "رمز فعلی را وارد نکردید" + "\n";
+        }
+        if(newPassword.equals("")){
+            errorMessages += "رمز جدید را وارد نکردید" + "\n";
+        }
+        if(repeatPassword.equals("")){
+            errorMessages += "تکرار رمز جدید را وارد نکردید" + "\n";
+        }
+        if(!newPassword.equals(repeatPassword)){
+            errorMessages += "رمز جدید و تکرار آن مطابقت ندارد" + "\n";
+        }
+        
+        if(errorMessages.equals("")){// everythings is right
+            final int accountId = (int) request.getSession().getAttribute("accountId");
+            AccountManagerImpl manager = new AccountManagerImpl();
+            Account account = manager.get(accountId);
+            if(account.getPassword().equals(curPassword)){
+                account.setPassword(newPassword);
+                manager.update(account);
+            }else{
+                errorMessages = "رمز فعلی صحیح نیست";
+                response.sendError(422, errorMessages);
+            }
+        }else{
+            response.sendError(422, errorMessages);
+        }        
+    }
+}
